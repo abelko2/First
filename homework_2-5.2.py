67dcbe9738d02ffa45914fc92610e9d0fba670de
abelko <abelko@debian.debian>
2018-11-02 09:22:46 -0400

    5.2b

---
+++ homework_2-5.2.py
@@ -0,0 +1,31 @@
+london_co = {
+'r1' : {
+'location': '21 New Globe Walk',
+'vendor': 'Cisco',
+'model': '4451',
+'ios': '15.4',
+'ip': '10.255.0.1'
+},
+'r2' : {
+'location': '21 New Globe Walk',
+'vendor': 'Cisco',
+'model': '4451',
+'ios': '15.4',
+'ip': '10.255.0.2'
+},
+'sw1' : {
+'location': '21 New Globe Walk',
+'vendor': 'Cisco',
+'model': '3850',
+'ios': '3.6.XE',
+'ip': '10.255.0.101',
+'vlans': '10,20,30',
+'routing': True
+}
+}
+r1 = input ('Enter device name: ')
+#r2 = input('Enter parameter name:')
+b = str(london_co[r1].keys ())
+b1 = b[11:-2]
+r2 = input('Enter parameter name ({} )'.format(b1))
+print(london_co[r1][r2])
