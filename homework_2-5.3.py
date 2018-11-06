1e87befc8166f6eb455119df8cdb7f44f5b20ba8
abelko <abelko@debian.debian>
2018-11-06 08:05:22 -0500

    5-3a

--- homework_2-5.3.py
+++ homework_2-5.3.py
@@ -8,13 +8,15 @@ trunk_template = ['switchport trunk encapsulation dot1q',
 'switchport trunk allowed vlan {}']
 i1 = input ('Enter interface mode (access/trunk):\n')
 i2 = input ('Enter interface type and number:\n')
-i3 = input ('Enter vlan(s):\n')
+itog = ['access','trunk']
+itog = {key:[] for key in itog}
+itog['access'] = 'Enter VLAN number:'
+itog['trunk'] = 'Enter allowed VLANs:'
+i3 = input ('{}\n'.format(itog[i1]))
 v1 = ','.join(access_template)
 v2 = ','.join(trunk_template)
 access_template = v1.replace(',', '\n')
 trunk_template = v2.replace(',', '\n')
-itog = ['access','trunk']
-itog = {key:[] for key in itog}
 itog['access'] = access_template
 itog['trunk'] = trunk_template
 a = itog[i1].format(i3)
@@ -23,4 +25,3 @@ k = '''
 {}
 '''
 print(k.format(i2,a))
-
