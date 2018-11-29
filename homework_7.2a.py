372fcabc8977461389ff04fbb3c757d9db6a6d3b
abelko <abelko@debian.debian>
2018-11-29 10:52:38 -0500

    7_2a

---
+++ homework_7.2a.py
@@ -0,0 +1,21 @@
+from sys import argv
+config = argv[1]
+#Передача через argv в скприпт того, что не будет выводиться в sh_run
+ignore = argv[2:]
+with open ('config_sw1.txt') as f:
+	for line in f:
+		if line.startswith('!'):
+			continue
+		else:
+			for line2 in ignore:
+				a = line2 in line
+				if a == True:
+					break
+				else:
+					continue
+			else:
+				if a == True:
+					continue
+				else:
+					print(line)
+
