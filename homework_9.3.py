8a9f572c2522c6c6205cfec26cc5ce916bcc0fe6
abelko <abelko@debian.debian>
2018-12-16 14:26:38 -0500

    9.30

---
+++ homework_9.3.py
@@ -0,0 +1,39 @@
+result_access = {}
+result_trunk = {}
+
+def get_int_vlan_map(cfg):
+	with open(cfg) as f:
+		for line in f:
+			if 'Ethernet' in line:
+				intf = line.rstrip()
+			elif 'access vlan' in line:
+				a = line
+				a = a.split()
+				for line2 in a:
+					if line2.isdigit():
+						n = int(line2)
+					else:
+						continue
+				result_access[intf] = n
+			elif 'trunk allowed' in line:
+				a = line
+				a = a.split()
+				for line2 in a:
+					if line2[1][-1].isdigit():
+						n1 = line2.split(',')
+						n2 = []
+						for line3 in n1:
+							k = int(line3)
+							n2.append(k)
+					else:
+						continue
+				result_trunk[intf] = n2
+			else:
+				continue
+	return result_access
+	return result_trunk
+
+get_int_vlan_map('config_sw1.txt')
+
+print(result_access)
+print(result_trunk)
